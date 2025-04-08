6a9f939fa6d791fc6a2e342fc53132abfbe9d58d
YassineElCode <yassineCodePY@hotmail.com>
2025-04-08 21:35:44 +0200

    added filament but still get an error, go to /admin and look at console

--- database/migrations/2025_04_08_192906_create_order_items.php
+++ database/migrations/2025_04_08_192906_create_order_items.php
@@ -18,7 +18,6 @@ return new class extends Migration {
             $table->decimal('price_at_time', 8, 2); // store snapshot of price
             $table->timestamps();
         });
-
     }
 
     /**
