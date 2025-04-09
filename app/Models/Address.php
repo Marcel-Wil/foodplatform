b30332d2fc8fc26b9bcb677502ae527f25153cdf
Marcel-wil <wilczynskimarceli@gmail.com>
2025-04-09 02:32:41 +0200

    add relationship in address.php

--- app/Models/Address.php
+++ app/Models/Address.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Address extends Model
 {
@@ -16,4 +17,10 @@ class Address extends Model
         'zipcode',
         'country',
     ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
 }
