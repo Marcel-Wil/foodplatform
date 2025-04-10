e41d2de953ca86a998a120d220eafd548713ad51
Marcel-wil <wilczynskimarceli@gmail.com>
2025-04-10 13:25:19 +0200

    change layout

--- resources/js/layouts/main-layout.tsx
+++ resources/js/layouts/main-layout.tsx
@@ -3,10 +3,14 @@ import Navbar from '@/components/navbar';
 
 export default function Layout({ children }) {
     return (
-        <main>
+        <div className="flex min-h-screen flex-col">
             <Navbar />
-            <article>{children}</article>
+
+            <main className="flex-grow">
+                <article>{children}</article>
+            </main>
+
             <Footer />
-        </main>
+        </div>
     );
 }
