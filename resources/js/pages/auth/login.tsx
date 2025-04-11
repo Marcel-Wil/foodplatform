284817feaf141859c1cc0bbd1697c484e42dbe24
Marcel-wil <wilczynskimarceli@gmail.com>
2025-04-11 06:40:45 +0200

    connect Login with backend + install checkbox/label shadcn components

--- resources/js/pages/auth/login.tsx
+++ resources/js/pages/auth/login.tsx
@@ -1,20 +1,59 @@
+import { Checkbox } from '@/components/ui/checkbox';
+import { Label } from '@/components/ui/label';
 import Layout from '@/layouts/main-layout';
+import { useForm } from '@inertiajs/react';
 
 const Login = () => {
+    const { data, setData, post, processing, errors } = useForm({
+        email: '',
+        password: '',
+        remember: false,
+    });
+
+    function submit(e) {
+        e.preventDefault();
+        post('/login');
+    }
+
     return (
         <div className="pt-44">
             <div className="mx-auto mb-12 w-full max-w-xl rounded-md bg-[#FAF2DB] p-10 shadow-md">
                 <h1 className="mb-6 text-center text-2xl font-bold">LOGIN</h1>
 
-                <form className="space-y-4">
+                <form className="space-y-4" onSubmit={submit}>
                     <div>
                         <label className="mb-1 block text-sm font-medium">Email</label>
-                        <input type="email" className="w-full rounded-sm border border-black bg-white px-3 py-2" required />
+                        <input
+                            type="email"
+                            className="w-full rounded-sm border border-black bg-white px-3 py-2"
+                            required
+                            onChange={(e) => setData('email', e.target.value)}
+                            value={data.email}
+                        />
+                        {errors.email && <div className="text-red-600">{errors.email}</div>}
                     </div>
 
                     <div>
                         <label className="mb-1 block text-sm font-medium">Password</label>
-                        <input type="password" className="w-full rounded-sm border border-black bg-white px-3 py-2" required />
+                        <input
+                            type="password"
+                            className="w-full rounded-sm border border-black bg-white px-3 py-2"
+                            required
+                            onChange={(e) => setData('password', e.target.value)}
+                            value={data.password}
+                        />
+                        {errors.password && <div className="text-red-600">{errors.password}</div>}
+
+                        <div className="mt-4 flex items-center space-x-3">
+                            <Checkbox
+                                id="remember"
+                                name="remember"
+                                checked={data.remember}
+                                onClick={() => setData('remember', !data.remember)}
+                                tabIndex={3}
+                            />
+                            <Label htmlFor="remember">Remember me</Label>
+                        </div>
                         <div className="mt-1 text-sm">
                             <a href="#" className="text-gray-600 underline">
                                 Forgot your password?
