9a5ba1340210e1d0a5375afab331607e44b542c4
YassineElCode <yassineCodePY@hotmail.com>
2025-04-12 19:50:07 +0200

    added cart and cartitem to filament

---
+++ app/Filament/Resources/CartItemResource.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\CartItemResource\Pages;
+use App\Filament\Resources\CartItemResource\RelationManagers;
+use App\Models\CartItem;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class CartItemResource extends Resource
+{
+    protected static ?string $model = CartItem::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    protected static ?string $navigationGroup = 'Carts';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\Select::make('cart_id')
+                    ->relationship('cart', 'id')
+                    ->required(),
+                Forms\Components\Select::make('foodItem_id')
+                    ->relationship('foodItem', 'name')
+                    ->required(),
+                Forms\Components\TextInput::make('quantity')
+                    ->required()
+                    ->numeric(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('cart.id')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('foodItem.name')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('quantity')
+                    ->numeric()
+                    ->sortable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListCartItems::route('/'),
+            'create' => Pages\CreateCartItem::route('/create'),
+            'edit' => Pages\EditCartItem::route('/{record}/edit'),
+        ];
+    }
+}
