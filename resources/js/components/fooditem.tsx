0f3cd99997a6fc66a5bb1e5fceb49d2e38853b84
YassineElCode <yassineCodePY@hotmail.com>
2025-05-12 16:03:11 +0200

    added object-cover to images in menu

--- resources/js/components/fooditem.tsx
+++ resources/js/components/fooditem.tsx
@@ -26,7 +26,7 @@ const Fooditem = ({ foodItem }) => {
             <img
                 src={foodItem.media.length > 0 ? foodItem.media[0].original_url : ''}
                 alt={foodItem.name}
-                className="h-full w-full transition-opacity duration-300 group-hover:opacity-0"
+                className="h-full w-full transition-opacity duration-300 group-hover:opacity-0 object-cover"
             />
             {/* Overlay content that becomes visible on hover */}
             <div className="absolute inset-0 flex flex-col items-center justify-center opacity-0 transition-opacity duration-300 group-hover:opacity-100">
